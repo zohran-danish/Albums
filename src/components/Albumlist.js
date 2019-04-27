0e09d231b1da89ba94c8614aaac34b54476a4090
Zohran Danish <danymast1@gmail.com>
2019-04-27 20:11:12 +0530

    finalizing app

--- src/components/Albumlist.js
+++ src/components/Albumlist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import  AlbumDetail  from "./AlbumDetail";
 import Card from './Card';
@@ -17,9 +17,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
